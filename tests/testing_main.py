35efac6ac566f49c9c1a0bdad4e58224b8f84888
Angelina T <156047645+bitebitbytebeta@users.noreply.github.com>
2025-05-07 06:24:02 +0000

    adding subtract function, and testing files

---
+++ tests/testing_main.py
@@ -0,0 +1,9 @@
+import sys
+import os
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
+from main import add
+
+def test_add():
+    assert add(2, 3) == 5
+    assert add(-1, 1) == 0
+    assert add(0, 0) == 0
---
+++ tests/testing_sub.py
@@ -0,0 +1,9 @@
+import sys
+import os
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
+from sub import sub
+
+def test_sub():
+    assert sub(2, 3) == -1
+    assert sub(-1, 1) == -2
+    assert sub(0, 0) == 0
