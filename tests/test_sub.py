fd81749dd97014c535c1ff424d049957874db29b
Angelina T <156047645+bitebitbytebeta@users.noreply.github.com>
2025-05-07 07:19:37 +0000

    moving back to folder, and fixing import and main

--- tests/test_sub.py
+++ tests/test_sub.py
@@ -1,3 +1,5 @@
+import unittest
+
 import sys
 import os
 sys.path.append(os.path.dirname(os.path.abspath(__file__)))
@@ -7,3 +9,6 @@ def test_sub():
     assert sub(2, 3) == -1
     assert sub(-1, 1) == -2
     assert sub(0, 0) == 0
+
+if __name__ == "__main__":
+    unittest.main()
